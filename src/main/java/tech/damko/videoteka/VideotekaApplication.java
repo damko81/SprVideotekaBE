6561b69e3f894daf3ea49b5ac1e58d584461154c
Damjan <damko81@gmail.com>
2024-04-09 23:33:58 +0200

    Authentication, base security, login/logout modul

--- src/main/java/tech/damko/videoteka/VideotekaApplication.java
+++ src/main/java/tech/damko/videoteka/VideotekaApplication.java
@@ -1,12 +1,24 @@
 package tech.damko.videoteka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tech.damko.videoteka.service.FilesStorageService;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class VideotekaApplication {
+public class VideotekaApplication implements CommandLineRunner {
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideotekaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+//    storageService.deleteAll();
+		storageService.init();
 	}
 }
---
+++ src/main/java/tech/damko/videoteka/response/ResponseMessage.java
@@ -0,0 +1,26 @@
+package tech.damko.videoteka.response;
+
+public class ResponseMessage {
+    private String message;
+
+    public ResponseMessage() { }
+
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseMessage{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
